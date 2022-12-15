21ff0d9154212986364b6c5b3c9d6be48d8ec7dd
Sreeraj R <sreeraj.r.0801@gmail.com>
2022-12-15 10:40:04 +0530

    First commit

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,3 @@
 export const baseUrl = 'https://api.themoviedb.org/3';
-export const API_key = "yor Api key";
+export const API_key = "8a24f4d21a9904e34d006a06433cb830";
 export const imageUrl = 'https://image.tmdb.org/t/p/original';
